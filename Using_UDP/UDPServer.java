04761bd180c79d4f55a298f16b8cfee3a4948a02
Prakriti Timilsina <98209027+Dynamic-prakriti@users.noreply.github.com>
2024-12-25 19:16:15 +0545

    Update UDPServer.java

--- Using_UDP/UDPServer.java
+++ Using_UDP/UDPServer.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class UDPServer {
     public static void main(String[] args) throws IOException{
         byte buf[] = new byte[256];
-        DatagramSocket socket = new DatagramSocket(9999);
+        DatagramSocket socket = new DatagramSocket([port]);
 
         DatagramPacket Packet = new DatagramPacket(buf,buf.length);
         socket.receive(Packet);
